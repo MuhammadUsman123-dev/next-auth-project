a3803a41f295aa3d59529adab2605da0eebc8089
MuhammadUsman123-dev <usmanhamayun70@gmail.com>
2024-12-10 14:56:32 +0500

    changing

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,10 +2,12 @@
 
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
   const logout = () => {
     try {
       axios.get("/api/users/logout");
@@ -16,11 +18,16 @@ export default function ProfilePage() {
       toast.error(error.message);
     }
   };
+  const getUserdetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data.data._id);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p className="text-4xl">Profile Page</p>
+      <h2>{data === "nothing" ? "" : ""}</h2>
       <hr />
       <button
         onClick={logout}
